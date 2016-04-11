d7092c6035f191cb0a91ca1636bd2752e0d499a8
Jonah BU <bu.jonah@gmail.com>
2016-04-11 19:06:22 +0800

    first run

---
+++ AmazonEmployeeAccess/RF.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+import numpy as np
+import pandas as pd
+
+
+f = open("./Data/train.csv")
+df = pd.read_csv(f, header = 0)
+
+# put the original column names in a python list
+original_headers = list(df.columns.values)
+
+# remove the non-numeric columns
+# df = df._get_numeric_data()
+
+# put the numeric column names in a python list
+numeric_headers = list(df.columns.values)
+print(numeric_headers)
+# create a numpy array with the numeric values for input into scikit-learn
+data = df.as_matrix()
+
+
+X = data[:, 1:]  # select columns 1 through end
+y = data[:, 0]   # select column 0, the stock price
+print(X)
+print('------------------------------')
+print(y)
+print('------------------------------')
+
+
+rfc = RandomForestClassifier(n_estimators=100,oob_score=True)
+rfc.fit(X,y)
+
+testf = open("./Data/test.csv")
+dtestf = pd.read_csv(testf, header = 0)
+numeric_headers = list(df.columns.values)
+data = df.as_matrix()
+testX = data[:, 1:]
+texty = rfc.predict(testX)
+print('result:')
+print(texty)
+print('sum: ',sum(texty), 'lenth', len(texty))
+print('oob_score: ',rfc.oob_score_)
